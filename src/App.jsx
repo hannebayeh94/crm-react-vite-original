ac4247dea05fe7b6f84181f9bb973b1ee26370f7
Hanne Bayeh <hannejosebayehvillarreal@gmail.com>
2022-07-22 20:49:53 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './layout/Layout'
+import Inicio from './paginas/Inicio'
+import NuevoCliente from './paginas/NuevoCliente'
+import EditarCliente from './paginas/EditarCliente'
+import VerCliente from './paginas/VerCliente'
+
+
+function App() {
+
+
+    console.log(import.meta.env);
+    console.log('hola');
+
+
+  return (
+      <BrowserRouter>
+          <Routes>
+              <Route path="/clientes" element={<Layout />}>
+                  <Route index element={<Inicio />} />
+                  <Route path="nuevo" element={<NuevoCliente />} />
+                  <Route path="editar/:id" element={<EditarCliente />} />
+                  <Route path=":id" element={<VerCliente />} />
+              </Route>
+          </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default App
